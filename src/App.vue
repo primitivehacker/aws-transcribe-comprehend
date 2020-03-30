2bb631f308e507f85ed6f19781c3633b3aff68c8
Derek Johnson <derek.johnson@chghealthcare.com>
2020-03-29 21:35:50 -0600

    final prototype of aws comprehend

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="section">
+          <upload />
+  </div>
+</template>
+
+<script>
+import upload from './upload.vue'
+export default {
+  name: 'App',
+  components: {upload}
+}
+</script>
